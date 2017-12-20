d12b15e12b5cf24e2d5f4408f8387b2fbd24dca8
tonquangtu <tonquangtu49@gmail.com>
2017-12-20 23:08:18 +0700

    add receive data from github

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/get-notify', (req, res) => {
+
+  const data = req.body;
+
+  res.json(data);
+  console.log(data);
+});
+
 module.exports = router;
